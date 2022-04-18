3a8b8efef7347f0d35c2a2fc6bfe01db7da2d02a
DafneM <moretti.dafne2@gmail.com>
2022-04-18 12:36:47 -0300

    Muda estilo da página

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 // import Caminhao from './img/caminhao.png';
 import Van from './img/van.png';
+import Logo from './img/truck.svg';
 import knapsack from './Knapsack';
 import ListItems from './Components/ListItems';
-import { Alert } from "react-bootstrap";
+import { Alert, NavbarBrand } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 
 function Home() {
   const [pesos, setPesos] = useState([0]);
@@ -20,6 +22,9 @@ function Home() {
 
   return (
     <div className="App">
+      <Navbar bg="navbar" variant="dark" sticky="top">
+        <NavbarBrand style={{ padding: '20px'}}><img src={Logo} style={{padding: '20px'}}/>Rico Truck</NavbarBrand>
+      </Navbar>
       <div className="first">
         <div className="imagens">
         {/* <img
@@ -42,11 +47,12 @@ function Home() {
           />
           </div>
           <div className="explanation">
-            <div className="title">Ajudamos sua transportadora a enviar seus carregamentos de modo mais <div style={{color: 'blue'}}>EFICIENTE</div></div>
-            <div className="subtitle">A ... calcula quais carregamentos devem ser enviados visando priorizar as entregas que
+            <div className="title">Ajudamos sua transportadora a enviar seus carregamentos de modo mais <div style={{color: '#7F63CF', fontFamily: 'Courier New', fontSize: '40px', marginTop: '30px', fontWeight: '900' }}>EFICIENTE</div></div>
+            <div className="subtitle">A Rico Truck calcula quais carregamentos devem ser enviados visando priorizar as entregas que
             devem ser feitas com mais urgência, analisando também o peso de cada encomenda e o peso do meio de transporte a ser analisado.</div>
           </div>
         </div>
+      <div style={{color: '#636060', fontFamily: 'Helvetica', fontSize: '20px', fontWeight: '800', marginTop: '80px', height: '100px', textAlign: 'left', marginLeft: '30px' }}>Informe a carga máxima permitida pelo caminhão que deseja consultar, adicione os itens e valores que o caminhão deverá carregar e veja o resultado rapidamente.</div>
       <div className="bottom-div">
         <MaxWeight
           setMaxWeight={setMaxWeight}
@@ -60,6 +66,8 @@ function Home() {
           setNomes={setNomes}
         />
       </div>
+      <div style={{color: '#636060', fontFamily: 'Helvetica', fontSize: '20px', fontWeight: '800', marginTop: '10px', height: '100px', textAlign: 'left', marginLeft: '30px' }}>Acompanhe a lista de itens já adicionados</div>
+
       <ListItems
         nomes={nomes}
         valores={valores}
@@ -77,7 +85,7 @@ function Home() {
                   O valor máximo possível de ser transportado no seu caminhão é R${result}
               </p>
           </Alert>
-          : <Button onClick={()=> {setShowAlert(true); knapsack({valores, pesos, maxWeight, setResult})}}>Calcular</Button>
+          : <Button onClick={()=> {setShowAlert(true); knapsack({valores, pesos, maxWeight, setResult})}} style={{marginBottom: '15px'}}>Calcular</Button>
       }
     </div>
   );
